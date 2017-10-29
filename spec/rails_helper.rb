71c92348314e9673d28504d56f1bfcafc44f9615
Mateusz Woskowicz <mateusz.woskowicz@enginearch.com>
2017-10-29 16:16:39 +0100

    rspec fix

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,12 +4,13 @@ require 'database_cleaner'
 require 'spec_helper'
 
 
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
-    with.library :rails
-  end
-end
+
+# Shoulda::Matchers.configure do |config|
+#   config.integrate do |with|
+#     with.test_framework :rspec
+#     with.library :rails
+#   end
+# end
 
 # [...]
 RSpec.configuration do |config|
